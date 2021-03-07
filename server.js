df2dae7b6d855c498b167fdbfff310fbf0137de4
cory neel <corycneel@gmail.com>
2021-03-06 19:16:18 -0600

    added GET and POST functions to serverjs

--- server.js
+++ server.js
@@ -12,8 +12,31 @@ app.use(express.json());
 app.use(express.static("public"));
 
 
-//TODO --- set up commands to pull info from db.json
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "/public/index.html"));
+});
 
+app.get("/notes", function (req, res) {
+  res.sendFile(path.join(__dirname, "/public/notes.html"));
+});
+
+app.post("/api/notes", function (req, res) {
+  fs.readFile(__dirname + "/db/db.json", 'utf8', function (error, notes) {
+    if (error) {
+      return console.log(error)
+    }
+    notes = JSON.parse(notes)
+
+    var id = notes[notes.length - 1].id + 1
+    var newNote = { title: req.body.title, text: req.body.text, id: id }
+    var activeNote = notes.concat(newNote)
+
+    fs.writeFile(__dirname + "/db/db.json", JSON.stringify(activeNote), function (error, data) {
+      if (error) {
+      res.json(activeNote);
+    })
+  })
+})
 
 // server listens
 app.listen(PORT, function () {
